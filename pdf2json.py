51eb0b8b1a5e2ffeba317e192e0b0d77814407ae
asearer <amsearer84@gmail.com>
2023-07-15 07:39:02 -0400

    On branch master  Changes to be committed: 	modified:   pdf2json.py Changed input/output from single file to directory to enable batch processing

--- pdf2json.py
+++ pdf2json.py
@@ -1,28 +1,43 @@
+import os
 import PyPDF2
 import json
 
-def pdf_to_json(pdf_path, json_path):
-    pdf_file = open(pdf_path, 'rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
+def pdf_to_json(input_dir, output_dir):
+    # Create output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    content = ''
-    for page in pdf_reader.pages:
-        content += page.extract_text()
+    # Iterate over PDF files in the input directory
+    for filename in os.listdir(input_dir):
+        if filename.endswith('.pdf'):
+            pdf_path = os.path.join(input_dir, filename)
 
-    pdf_file.close()
+            pdf_file = open(pdf_path, 'rb')
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
 
-    # Convert text content to JSON
-    json_data = {
-        'content': content
-    }
+            content = ''
+            for page in pdf_reader.pages:
+                content += page.extract_text()
 
-    with open(json_path, 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
+            pdf_file.close()
 
-    print(f'Successfully converted PDF to JSON. JSON file saved at {json_path}.')
+            # Convert text content to JSON
+            json_data = {
+                'content': content
+            }
 
-# Provide the path to the PDF file and the desired path for the JSON file
-pdf_file_path = 'path/to/input.pdf'
-json_file_path = 'path/to/output.json'
+            # Save JSON file in the output directory
+            json_filename = os.path.splitext(filename)[0] + '.json'
+            json_path = os.path.join(output_dir, json_filename)
 
-pdf_to_json(pdf_file_path, json_file_path)
+            with open(json_path, 'w') as json_file:
+                json.dump(json_data, json_file, indent=4)
+
+            print(f'Successfully converted {filename} to JSON. JSON file saved at {json_path}.')
+
+# Provide the input and output directories
+input_directory = 'path/to/input_directory'
+output_directory = 'path/to/output_directory'
+
+pdf_to_json(input_directory, output_directory)
+
